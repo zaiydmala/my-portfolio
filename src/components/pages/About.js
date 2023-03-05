f7c93607afd2a18b1ce6a252d88005e8c39e183c
Zaiyd Mala <zaiydhmala@gmail.com>
2023-03-04 23:33:58 -0500

    Fix resume button for mobile screens

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,17 +19,18 @@ const About = () => {
           style={{ border: "3px solid #3d997d" }}
           alt="profile"
         />
-        <p className="max-w-s font-medium text-white">
-          I'm a passionate programmer that enjoys the <span>thrill</span> of
-          learning. I have a passion for creating interactive and minimalist web designs 
-          that are both visually appealing and easy to use.
-          I love applying responsive design principles and watching my web pages shrink into mobile screens and still look amazing. 
+        <p className="max-w-sm font-medium text-white sm:text-sm">
+          I'm a passionate programmer that enjoys the thrill of
+          learning. I have a passion to create interactive and minimalist web designs 
+          that are visually appealing and easy to use.
+          I love applying responsive design principles and watching my webpages shrink into mobile screens and still look amazing. 
           It feels oddly satisfying. I think in a way I am kind of like those web pages;<br/><br/>
           <span>I am moldable, but I still keep my creative flare intact🔥</span>
+          
         </p>
         
 
-        <p className="max-w-s font-medium text-white">  
+        <p className="max-w-sm font-medium text-white">  
           { /* I yearn to build products that solve real life problems, and
           have a positive impact in society at large. Talk to me if you need
           someone with the commitment and eagerness to push your product ahead. */ }
