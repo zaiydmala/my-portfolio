632ff1831107f904f559924873b73ac11d6279b5
Zaiyd Mala <zaiydhmala@gmail.com>
2023-03-19 18:37:53 -0400

    Update content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,12 +19,11 @@ const About = () => {
           style={{ border: "3px solid #3d997d" }}
           alt="profile"
         />
-        <p className="max-w-sm font-medium text-white sm:text-sm">
-          I'm a programmer that enjoys the thrill of
-          learning with a passion to create interactive and minimalist web designs 
-          that are visually appealing and easy to use.
-          I love applying responsive design principles and watching my webpages shrink into mobile screens and still look amazing. 
-          It feels oddly satisfying. I think in a way I am kind of like those web pages;<br/><br/>
+        <p className="max-w-xs font-medium text-white sm:text-sm">
+          I'm a programmer who enjoys meeting new people. 
+          I yearn to build products that solve real-life problems and have a positive impact in society.
+          I'm a fan of applying responsive design principles and watching my webpages shrink into mobile screens yet still look amazing. 
+          It feels oddly satisfying. In a way I am kind of like those web pages;<br/><br/>
           <span>I am moldable, but I still keep my creative flare intact 🔥</span><br/><br/>
           Click here for my{" "}
           <span>
